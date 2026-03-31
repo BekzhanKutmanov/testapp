1a4815d527ea4eb15714d59994342ffc55a65f64
BekzhanKutmanov <kutmanov@gmail.com>
2026-03-31 11:59:17 +0600

    Кокомпонент toast гÑотÐов, изменил общÑий вцвет проекта

--- src/shared/api/QueryClientProvider.tsx
+++ src/shared/api/QueryClientProvider.tsx
@@ -1,23 +1,66 @@
 // shared/api/QueryClientProvider.tsx
 'use client'
 
-import { useState } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react';
+
+import { QueryClient, QueryClientProvider, MutationCache, QueryCache } from '@tanstack/react-query';
+import { SnackbarProvider, enqueueSnackbar } from 'notistack';
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
   // Создаем QueryClient один раз и храним его в state
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false, // опционально: отключаем перезапрос при смене вкладки
-        retry: 1,
-      },
-    },
-  }))
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          // Глобальный перехват ошибок из твоего apiRequest
+          onError: (error: any) => {
+            enqueueSnackbar(error.message || 'Ошибка', { variant: 'error' })
+          }
+        }),
+        mutationCache: new MutationCache({
+          onError: (error: any) => {
+            enqueueSnackbar(error.message || 'Ошибка при выполнении действия', { variant: 'error' });
+          },
+        }),
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false, // опционально: отключаем перезапрос при смене вкладки
+            retry: 1,
+            throwOnError: false,
+          }
+        }
+      })
+  )
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+        {children}
+      </SnackbarProvider>
     </QueryClientProvider>
   )
+
+  // ручной
+  // const handleClick = () => {
+  //   enqueueSnackbar('Действие выполнено!', { variant: 'success' });
+  // };
+
+  // с jsx
+  // enqueueSnackbar(
+  //   <div style={{ display: 'flex', flexDirection: 'column' }}>
+  //     <b style={{ color: '#fff' }}>Внимание!</b>
+  //     <span>Вы можете <a href="/settings" style={{ color: 'yellow' }}>настроить профиль</a> прямо сейчас.</span>
+  //   </div>,
+  //   {
+  //     variant: 'info',
+  //     autoHideDuration: 5000,
+  //   }
+  // );
+
+  // глобальный даже внутри функции
+  // import { enqueueSnackbar } from 'notistack';
+  // export const someUtilFunction = () => {
+  //   // Это сработает, если SnackbarProvider уже есть в дереве компонентов
+  //   enqueueSnackbar('Глобальное сообщение', { variant: 'warning' });
+  // }
 }
--- src/shared/api/test.tsx
+++ src/shared/api/test.tsx
@@ -3,5 +3,8 @@ import {apiRequest} from "@/shared/api/apiHelper";
 
 export const test = async ()=> {
   const data = await apiRequest(axiosInstance.get(''));
+
+  console.log(data);
+
   return data;
 }
