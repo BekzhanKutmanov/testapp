85f4ce665b350117cc3ae2d0064b7d5fbef22763
BekzhanKutmanov <kutmanov@gmail.com>
2026-04-08 17:27:31 +0600

    установил кредакс тулкит

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,6 +9,7 @@ import ThemeProvider from '@components/theme'
 // Util Imports
 import { getMode, getSettingsFromCookie } from '@core/utils/serverHelpers'
 import QueryProvider from "@/shared/api/QueryClientProvider";
+import StoreProvider from '@/features/StoreProvider'
 
 type Props = ChildrenType & {
   direction: Direction
@@ -25,11 +26,13 @@ const Providers = (props: Props) => {
   return (
     <VerticalNavProvider>
       <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
-        <ThemeProvider direction={direction}>
-          <QueryProvider>
-            {children}
-          </QueryProvider>
-        </ThemeProvider>
+        <StoreProvider>
+          <ThemeProvider direction={direction}>
+            <QueryProvider>
+              {children}
+            </QueryProvider>
+          </ThemeProvider>
+        </StoreProvider>
       </SettingsProvider>
     </VerticalNavProvider>
   )
